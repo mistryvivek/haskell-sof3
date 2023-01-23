84dc829eba87f00c9fa3ec5ec39ac5a51d34f8ba
Vivek <mistry.vivek03@gmail.com>
2023-01-23 15:20:17 +0000

    Reached extension tasks

--- Block1Q.hs
+++ Block1Q.hs
@@ -1,3 +1,40 @@
+{-
+Q1. Evaluating Simple Expressions
+1. 3+8=11
+2. 3^2=9
+3. 2^3^4==(2^3)^4 - FALSE
+4. 2^3^4==2^(3^4) - TRUE
+5. 0==1 || 0/=1 - TRUE
+6: "h" : "ello" - charactor concat with string (Hello)
+7. 7 : [6, 5, 4] - list concat
+8. if 0==1 then "Alice" else "Bob" - Bob
+9. let sqr = \ n -> n^2 in sqr 3 + sqr 4 - Using a Lamba function to define squares before doing something.
+10. let cube n = n^3 in cube 2 + cube 3 - Using a function to define cubes and using it in an expression.
+
+Q3. Detecting Errors
+1.if 0 then "a" else "b" - if clause doesn't contain anything to be mapped to boolean.
+2.if "a" == "b" then 5 - must use if/then/else pattern in Haskell.
+3.if "a" = "b" then 5 else 4 - singleton equals doesn't work for equality.
+4. if 0 == 0 then 5 else 'b' - must return an int or a string
+5. ["0", 5, 9] - List of multiple types cannot exist.
+6. ("a", 42 - Missing closing parenthesis.
+7. let sumSq m n = m^2 - n^2 in sumSq 3 4 - Plus instead of the minus.
+8. let f (x:xs) = 42 in f [] - need to define a pattern for the empty list.
+9. let f n = if n == 0 then 0 else f (n+1) in f 3 - No reachable base case.
+10. * 2 3 - Need bracket around the multiplication.
+11. "3333" :: Int - Its a string!
+
+Q4. Simple expressions that use values defined in Prelude
+1. fst, snd - First and second value from given tuple.
+2. curry, uncurry - Pack and unpack a tuple input.
+3. succ, pred - Next and previous value (+/- 1: numeric values)
+4. negate, abs, signum - change sign, get value w/o sign and
+get sign.
+5. quot, rem, div, mod, divMod - division related functions.
+6. gcd - greater common divisor
+7. id, const, (.) - returns itself
+-}
+
 import Prelude
 
 greet :: String -> String
@@ -70,7 +107,7 @@ incList (n:ns) = [n + 1] ++ incList ns
 incList' :: [Int] -> [Int]
 incList' a = map (+1) a
 
-greetTest' = ""
+greetTest' = map (greet . fst) testData == map snd testData
   where testData = [("Kofi", "Hello Kofi!"),
                      ("Jeremy", "Hello Jeremy!"),
                      ("", "Hello !")]
@@ -81,8 +118,19 @@ pos x y = if elem heady x
           else 1 + pos x (tail y)
    where heady = head y
 
+insert :: Ord a => a -> [a] -> [a]
+insert b [] = [b]
+insert b c = if b < head c
+             then [b] ++ c
+             else [head c] ++ insert b (tail c)
 
+isort :: Ord a => [a] -> [a]
+isort a = foldr insert [] a
 
+insert' :: Ord a => a -> [a] -> [a]
+insert' x = foldr
+  where
+    insx
 
 ----
 
