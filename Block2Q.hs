ce4b5f3e4244dbdd6ad8b67d65a89daad3b38baf
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-05 11:41:11 +0000

    Continued exercises for Block 2

--- Block2Q.hs
+++ Block2Q.hs
@@ -26,7 +26,8 @@ onetwo' css@(c:ss) = onetwoassist c ++ onetwo' ss
 {-Takes a boolean list and converts to boolean.
 Starts on the left, takes in a lamba function with parameters n and b which does 2*n + 1(if B is true).
 
-Parity takes a boolean list and returns a boolean.
+Parity takes a boolean list and returns a boolean. Parity returns true if there is a even number of trues in
+the list.
 
 The main function applies the function bitstring2int to every filtered subset according to parity.-}
 bitstring2int :: [Bool] -> Int
@@ -35,13 +36,41 @@ bitstring2int = foldl (\ n b -> 2*n + if b then 1 else 0) 0
 parity :: [Bool] -> Bool
 parity = (==0) . (`mod` 2) . length . filter (== True) 
 
-onethree, onethree' :: [[Bool]] -> [Int]
+onethree ,onethree' :: [[Bool]] -> [Int]
 onethree = map bitstring2int . filter parity
-onethree' = undefined
+onethree' css = [(bitstring2int cs)|
+          cs <- css, parity cs == True]
 
 data Maybe a = Nothing | Just a
 
+ePbs2i :: [Bool] -> Int
+ePbs2i bs | parity bs = bitstring2int bs
+{- Anything false according to parity is not returned.-}
 
+{-Q2.2: Stuck-}
 
+{-Q3.1:
 
+(if True then inc else dbl) (2+1)
+Inc (2+1)
+Inc 3-}
+
+ones, nats :: [Integer]
+ones = 1 : ones
+nats = 0 : map succ nats
+
+{-Base case:
+
+One: 1
+Nats: 0
+
+Recursive Case:
+
+Ones: ": ones"
+Nates: ": map succ nats"-}
+
+ackermann :: Integer -> Integer -> Integer
+ackermann 0 n = n+1
+ackermann m 0 = ackermann (m-1) 1
+ackermann m n = ackermann (m-1) (ackermann m (n-1))
 
