8a42ed693ff6e777e0b1c41c27ebdd67716373ab
mistryvivek <85026959+mistryvivek@users.noreply.github.com>
2023-02-09 15:03:52 +0000

    Week 5 Haskell

--- Block2Q.hs
+++ Block2Q.hs
@@ -1,23 +1,27 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 import Prelude
+import Control.Exception (bracketOnError)
 
 {- Any number that is even is added to one.-}
 oneone, oneone' :: [Int] -> [Int]
-oneone ns = [ n+1 | n <- ns,  n `mod` 2 == 0 ]
+oneone ns = [ n+1 | n <- ns,  even n]
 oneone' [] = []
-oneone' ns = if (head ns) `mod` 2 == 0
-             then [head ns + 1] ++ oneone' rest
-             else [] ++ oneone' rest
+oneone' ns = if even (head ns)
+             then (head ns + 1) : oneone' rest
+             else oneone' rest
    where rest = tail ns
 
 {- Mod index value by 2 and returns if true for all lower case letters-}
 onetwo, onetwo' :: [String] -> [Bool]
-onetwo css = [(fromEnum c) `mod` 2 == 1 |
+onetwo css = [fromEnum c `mod` 2 == 1 |
               cs <- css, length cs > 1, c <- cs, c `elem` ['a'..'z']]
 
 onetwoassist :: String -> [Bool]
 onetwoassist [] = []
 onetwoassist str@(s:tr) = if s `elem` ['a'..'z']
-                          then [(fromEnum s) `mod` 2 == 1] ++ onetwoassist tr
+                          then (fromEnum s `mod` 2 == 1) : onetwoassist tr
                           else onetwoassist tr
 
 onetwo' [] = []
@@ -38,8 +42,8 @@ parity = (==0) . (`mod` 2) . length . filter (== True)
 
 onethree ,onethree' :: [[Bool]] -> [Int]
 onethree = map bitstring2int . filter parity
-onethree' css = [(bitstring2int cs)|
-          cs <- css, parity cs == True]
+onethree' css = [bitstring2int cs|
+          cs <- css, parity cs]
 
 ePbs2i :: [Bool] -> Int
 ePbs2i bs | parity bs = bitstring2int bs
@@ -59,9 +63,22 @@ doubleOddM :: Maybe Int -> Maybe Int
 doubleOddM Nothing = Nothing
 doubleOddM (Just o) | o `mod` 2 == 1 = Just (o * 2)
                     | otherwise = Nothing
-                    
+
+{- 
 doepM :: [Bool] -> String
-doepM a = maybe (bitstring2int a) 
+doepM a = maybe "Oops!" (bitstring2int a * 2)
+
+
+doepE :: [Bool] -> String
+doepE = undefined
+
+type Error a = Either String a
+
+doubleOddE :: Error Int -> Error Int
+doubleOddE  (Left msg) = 
+doubleOddE  (Right o) | o `mod` 2 == 1 = Right (o * 2)
+                       | otherwise = Left "Must be an odd number!"
+-}
 
 {-Q3.1:
 
@@ -83,8 +100,101 @@ Recursive Case:
 Ones: ": ones"
 Nates: ": map succ nats"-}
 
+fix :: (a->a) -> a
+fix    f       = f (fix f)
+
+ones' :: [Integer]
+ones' = fix (1:)
+
+nats' :: [Integer]
+nats' = fix (map succ .) 0
+
 ackermann :: Integer -> Integer -> Integer
 ackermann 0 n = n+1
 ackermann m 0 = ackermann (m-1) 1
 ackermann m n = ackermann (m-1) (ackermann m (n-1))
 
+{-
+ackermann' :: Integer -> Integer -> Integer
+ackermann' n m = fix 
+-}
+
+bottom :: a
+bottom = fix bottom
+
+findPlateau :: Eq a => [a] -> a
+findPlateau a | head a == head (tail a) = head a
+              | otherwise = findPlateau (tail a) 
+
+tz :: Int -> Int
+tz n = negate (n `div` 2)
+
+mystery3_5 :: Eq a => (a -> a) -> a -> a
+mystery3_5 = (findPlateau .) . iterate
+
+{-Mystery 3_5:
+
+returns an infinite list of repeated applications of f to x
+
+Return list with true or false, with all true values meaning same
+value is in the next position.
+
+-}
+
+mersenne :: [Int]
+mersenne = map (\n -> n * n - 1) [0..]
+
+eratosthenes :: [Int]
+eratosthenes = sieve (map Just [2..])
+  where
+    sieve (Just n  : ns) = n : sieve (zipWith ($) mark_n ns)
+      where mark_n = cycle (replicate (n-1) id ++ [const Nothing]) 
+    sieve (Nothing : ns) = sieve ns
+mersennePrime :: [Int]
+mersennePrime = [1,2]
+
+{-Look at solutions-}
+
+{-Q4-}
+
+newtype Tree a = Tree [(a, Tree a)] deriving Show
+
+data VM_Event = Coin | Choc | Fudj | Halt deriving (Eq, Show)
+
+type VM = Tree VM_Event
+
+vm1_2 :: VM
+vm1_2  = Tree [(Coin, Tree [(Choc, Tree [(Coin, Tree [(Choc, Tree [])])])])]
+
+vm1_e :: VM
+vm1_e = Tree [(Coin, Tree [(Choc, vm1_e)])] 
+
+leadsto :: a -> Tree a -> Tree a
+e `leadsto` b = Tree [(e, b)]
+
+branch :: Tree a -> Tree a -> Tree a
+Tree ts `branch` Tree us = Tree (ts ++ us)
+
+vm1_2' :: VM
+vm1_2' = Coin `leadsto` (Choc `leadsto` (Coin `leadsto` (Choc `leadsto` stop)))
+
+stop :: Tree a
+stop = Tree []
+
+vm1_e' :: VM
+vm1_e' = Coin `leadsto` vm1_e'
+
+vm1_h :: VM
+vm1_h = (Coin `leadsto` ((Choc `leadsto` vm1_h)
+                         `branch`
+                         (Halt `leadsto` stop)))
+        `branch`
+        (Halt `leadsto` stop)
+
+vm1_h' :: VM
+vm1_h' = Coin `leadsto` (Choc `leadsto` vm1_h')
+        `branch`
+        (Halt `leadsto` stop)
+
+takeTree :: Int -> Tree a -> Tree a
+takeTree = undefined
---
+++ CanIHaskall.hs
@@ -0,0 +1,84 @@
+module CanIHaskall where
+
+import Prelude
+import Data.Char (isNumber)
+import Data.ByteString (count)
+
+isHaskell :: String -> Bool
+isHaskell "Haskell" = True
+isHaskell _ = False
+
+testHaskell :: Bool
+testHaskell = (isHaskell "Haskell" == True) &&
+    (isHaskell "Software" == False) &&
+    (isHaskell "" == False) &&
+    (isHaskell "haskell" == False)
+
+lowerVowel :: String -> Bool
+lowerVowel s | null s = True
+    | head s `elem` ['a', 'e', 'i', 'o', 'u'] = lowerVowel (tail s)
+    | otherwise = False
+
+
+
+testlv:: Bool
+testlv = (lowerVowel "ue" == True) &&
+    (lowerVowel "ueA" == False) &&
+    (lowerVowel "uea" == True)
+
+testprodCube :: Bool
+testprodCube = (prodCube [] == 1)
+    && (prodCube [4, 8] == 1)
+    && (prodCube [4, 6, 8] == 216)
+    && (prodCube [4, 6, 8, 12] == 216)
+    && (prodCube [2..11] == 1728000)
+
+prodCube :: [Int] -> Int
+prodCube [] = 1
+prodCube (x:xs) = if (x `mod` 2 == 0 && x `mod` 4 > 0)
+                  then x^3 * prodCube xs 
+                  else prodCube xs 
+
+consDiff :: String -> Int
+consDiff [] = 0
+consDiff (x:xs) | x `elem` "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ" = 1 - consDiff xs
+                | isNumber x = 1 + consDiff xs 
+                | otherwise = consDiff xs
+
+testconsDiff :: Bool
+testconsDiff = (consDiff "" == 0)
+    && (consDiff "SOf3in2021" == -2)
+    && (consDiff "Software123andTheory123" == 5)
+    && (consDiff "HASkellprogramming2021" == 9)
+
+isIATA :: String -> Bool
+isIATA a = length (filter (==True) (map (\x-> x `elem` "BCDFGHJKLMNPQRSTVWXYZ") a)) == 3
+
+testisIATA :: Bool
+testisIATA =
+    (isIATA "" == False) &&
+    (isIATA "MAN" == False) &&
+    (isIATA "LHR" == True) &&
+    (isIATA "LHRT" == False) &&
+    (isIATA "lhr" == False) &&
+    (isIATA "JFK" == True) &&
+    (isIATA "BHX" == True)
+
+testcountIATA :: Bool
+testcountIATA =
+    (countIATA ["LHR"] == 1) &&
+    (countIATA ["LHR", "Lhr", "MAN", "JFK", "", "jfk"] == 2) &&
+    (countIATA ["LHR", "BHX", "MAN", "JFK", "ACC", "LRHT"] == 3)
+
+countIATA :: [String] -> Int
+countIATA a = length (filter (== True) (map isIATA a))
+
+type Students = [(Name, Age, College)]
+type Name = String
+type Age = Int
+type College = String
+
+onBus66 :: Students
+onBus66 = [("Zain", 18, "Halifax"), ("Julia", 20, "Constantine"), ("Mandy", 22, "Goodricke"), ("Jack", 24, "Constantine"),("Emma", 21, "Langwith"), ("Zack", 19, "Halifax"),("Alice", 21, "Halifax"), ("Bob", 19, "Alcuin"),("Lui", 22, "Goodricke")]
+
+
